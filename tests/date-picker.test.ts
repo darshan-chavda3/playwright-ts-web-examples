1257117add2c8c0a1c531ebbcd7595711af55882
darsh-chavda <dhchavda1931@gmail.com>
2024-04-06 14:50:33 +0530

    Added handle pages test

---
+++ tests/date-picker.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+import moment from "moment";
+
+test("date picker using moment", async ({ page }) => {
+  await page.goto("https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo");
+  await selectDate(14, "March 2025");
+
+  async function selectDate(date: number, monthYear: string) {
+    const mmYYYY = page.locator("//*[@class='datepicker-switch']").nth(0);
+    const prev = page.locator("//*[@class='prev']").nth(0);
+    const next = page.locator("//*[@class='next']").nth(0);
+    const thisMonth = moment(monthYear, "MMMM YYYY").isBefore();
+
+    await page.locator("//*[@placeholder='Start date']").click();
+    while ((await mmYYYY.textContent()) != monthYear) {
+      if (thisMonth) {
+        await prev.click();
+      } else {
+        await next.click();
+      }
+    }
+    await page.locator(`//td[@class='day'][text()='${date}']`).click();
+  }
+
+  await page.waitForTimeout(5000);
+});
