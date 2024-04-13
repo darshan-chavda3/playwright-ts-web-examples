6cdb337090b725db8ed19e47c82ab4004ec18d7a
darsh-chavda <dhchavda1931@gmail.com>
2024-04-13 19:17:02 +0530

    added new test case

--- tests/handle-pages.test.ts
+++ tests/handle-pages.test.ts
@@ -14,7 +14,25 @@ test("handle multiple pages", async ({ context }) => {
   // Assertions on new tab
   await expect(newTab).toHaveURL("https://demoqa.com/sample");
   await expect(newTab.locator("//h1[@id='sampleHeading']")).toHaveText("This is a sample page");
+  await newTab.waitForTimeout(5000);
+  await newTab.close();
   await page.waitForTimeout(5000);
+});
+
+test("handle new page without context", async ({ page }) => {
+  // Page setup
+  await page.goto("https://demoqa.com/browser-windows");
+  await page.locator("//*[@id='tabButton']").click();
+
+  // New Tab setup
+  const [newPage] = await Promise.all([page.waitForEvent("popup")]);
+  const newTab = newPage;
+  await newTab.waitForLoadState();
+
+  // Assertions on new tab
+  await expect(newTab).toHaveURL("https://demoqa.com/sample");
+  await expect(newTab.locator("//h1[@id='sampleHeading']")).toHaveText("This is a sample page");
+  await newTab.waitForTimeout(5000);
   await newTab.close();
   await page.waitForTimeout(5000);
 });
